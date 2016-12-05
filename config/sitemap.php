cbb0cfd9f89623c21f95821398528b76ca77441e
Andrey Helldar <helldar@ai-rus.com>
2016-12-05 15:40:07 +0000

    Apply fixes from StyleCI

--- config/sitemap.php
+++ config/sitemap.php
@@ -7,42 +7,42 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-return [
-    /**
+return array(
+    /*
      * Caching time in minutes.
      * Set `0` to disable cache.
      * Default: 0.
      */
-    'cache'             => 0,
+    'cache' => 0,
 
-    /**
+    /*
      * Age data in days, over which references will not be included in the sitemap.
      * Default: 180 days.
      */
-    'age'               => 180,
+    'age' => 180,
 
-    /**
+    /*
      * For some column search.
      * Default: updated_at.
      */
-    'age_column'        => 'updated_at',
+    'age_column' => 'updated_at',
 
-    /**
+    /*
      * This value indicates how frequently the content at a particular URL is likely to change.
      */
-    'frequency'         => 'daily',
+    'frequency' => 'daily',
 
-    /**
+    /*
      * The time the URL was last modified.
      * This information allows crawlers to avoid redrawing documents that haven't changed.
      */
     'last_modification' => true,
 
-    /**
+    /*
      * Models for searching data.
      */
-    'items'             => [
-        /**
+    'items' => array(
+        /*
          * `model` Model for data retrieval.
          * `route` Name of route for return URL.
          * `keys`  `Key for route` => `Column in database`
@@ -52,14 +52,14 @@ return [
          *            http://mysite.com/user/10/2 for `category_id` = 10 and `user_id` = 2
          * `priority` Priority for save sitemap. Default: 0.8
          */
-        [
-            'model'    => User::class,
-            'route'    => 'user::show',
-            'keys'     => [
+        array(
+            'model' => User::class,
+            'route' => 'user::show',
+            'keys' => array(
                 'category' => 'category_id',
-                'id'       => 'user_id',
-            ],
+                'id' => 'user_id',
+            ),
             'priority' => 0.8,
-        ],
-    ],
-];
+        ),
+    ),
+);
