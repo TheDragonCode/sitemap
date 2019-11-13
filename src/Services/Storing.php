aa2042b1b16dc10a6f828d6228413582fc83a8ff
Andrey Helldar <helldar@ai-rus.com>
2019-11-13 18:03:18 +0300

    Refactoring in progress [2]

--- src/Services/Storing.php
+++ src/Services/Storing.php
@@ -23,4 +23,14 @@ class Storing implements StorableContract
     {
         return $path ?: Config::get('filename', 'sitemap.xml');
     }
+
+    static private function saveOne(string $path, array $except = ['images']): bool
+    {
+        return Storage::put($path);
+    }
+
+    static private function saveMany(string $path): bool
+    {
+
+    }
 }
